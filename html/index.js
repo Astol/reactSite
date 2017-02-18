59ab50f0d689a181f88481d5bf62470f8a3816f1
alest170 <alest170@student.liu.se>
2017-02-18 16:23:15 +0100

    not empty

--- html/index.js
+++ html/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+$(document).ready(function() {
 
-ReactDOM.render(
-  <h1>Hello snygging</h1>,
-  document.getElementById('root')
-)
+  ReactDOM.render(
+    <h1>Hello snygging</h1>,
+    document.getElementById('root')
+  )
